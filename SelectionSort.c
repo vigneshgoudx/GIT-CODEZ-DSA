f95916d15cbd6507b75638d55516dd548b945eca
vigneshgoudx <vigneshdesham@gmail.com>
2023-07-27 23:34:05 +0530

    Summary
    Selection Sort in C

---
+++ SelectionSort.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void printArray(int* A, int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+
+void selectionSort(int *A, int n){
+    int indexOfMin, temp;
+    for (int i = 0; i < n-1; i++)
+    {
+        indexOfMin = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(A[j] < A[indexOfMin]){
+                indexOfMin = j;
+            }
+        }
+        temp = A[i];
+        A[i] = A[indexOfMin];
+        A[indexOfMin] = temp;
+    }
+}
+
+int main()
+{
+    int A[] = {8,3,1,13};
+    int n = 4;
+    printArray(A, n);
+    selectionSort(A, n);
+    printArray(A, n);
+
+    return 0;
+}
